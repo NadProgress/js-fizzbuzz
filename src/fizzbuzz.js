2e65be1bbd0aaf9462e1eb1445126b593c8569f1
Nkusi Aime Didier <nkuaime@gmail.com>
2024-04-26 19:34:12 +0200

    fizzbuzz functions

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,19 @@
 // TODO: Write your function in this file
-
+function numbers(low, up) {
+  let array = []
+  for (let i = low; i <= up; i++) {
+    if (i % 15 === 0) {
+      array.push('FizzBuzz')
+    } else if (i % 5 === 0) {
+      array.push('Buzz')
+    } else if (i % 3 === 0) {
+      array.push('Fizz')
+    } else {
+      array.push(i)
+    }
+  }
+  return array
+}
+console.log(numbers(2, 20))
 // TODO: Change undefined below to the name of your function
-module.exports = undefined
+module.exports = numbers
